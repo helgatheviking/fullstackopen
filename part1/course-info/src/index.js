26c32ca3ce1983724f2eba4245023ee3a67ca992
Kathy Daring <helgatheviking@gmail.com>
2019-10-07 12:14:32 -0600

    step4, refactor objects into an array

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Footer total_exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Footer parts={parts} />
     </div>
   )
 }
@@ -35,9 +37,9 @@ const Content = (props) => {
   console.log(props);
     return (
         <div>
-            <Part title={props.part1.name} num_exercises={props.part1.exercises} />
-            <Part title={props.part2.name} num_exercises={props.part2.exercises} />
-            <Part title={props.part3.name} num_exercises={props.part3.exercises} />
+            <Part title={props.parts[0].name} num_exercises={props.parts[0].exercises} />
+            <Part title={props.parts[1].name} num_exercises={props.parts[1].exercises} />
+            <Part title={props.parts[2].name} num_exercises={props.parts[2].exercises} />
         </div>
       )
 }
@@ -51,8 +53,10 @@ const Part = (props) => {
 }
 
 const Footer = (props) => {
+    let total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+
     return (
-          <p>Number of exercises {props.total_exercises}</p>
+          <p>Number of exercises {total}</p>
       )
 }
 
