c2b8ef16098bebe463e30258c7eca9043c3c3df3
Kathy Daring <helgatheviking@gmail.com>
2019-10-07 12:06:11 -0600

    step 3, modify variable definitions

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Footer total_exercises={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Footer total_exercises={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
@@ -26,11 +32,12 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props);
     return (
         <div>
-            <Part title={props.part1} num_exercises={props.exercises1} />
-            <Part title={props.part2} num_exercises={props.exercises2} />
-            <Part title={props.part3} num_exercises={props.exercises3} />
+            <Part title={props.part1.name} num_exercises={props.part1.exercises} />
+            <Part title={props.part2.name} num_exercises={props.part2.exercises} />
+            <Part title={props.part3.name} num_exercises={props.part3.exercises} />
         </div>
       )
 }
