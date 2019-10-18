aea929c76847fbff15922f46611206dd7d717427
Kathy Daring <helgatheviking@gmail.com>
2019-10-18 16:06:28 -0600

    2.1 - intial commit

---
+++ part2/course-info/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Course = ({course}) => {
+	return (
+		<div>
+			<Header name={course.name} />
+			<Content parts={course.parts} />
+		</div>
+	)
+}
+
+const Header = ({name}) => {
+	return (
+		  <h2>{name}</h2>
+	)
+}
+
+const Content = ({parts}) => {
+	return (
+		<div>
+		{parts.map((part, i) => {
+			return (<Part key={i} part={part} />)
+		})}
+		</div>
+	)
+}
+
+const Part = ({part}) => {
+	return (
+		<p>
+		  {part.name}: {part.exercises}
+		</p>
+	)
+}
+
+const App = () => {
+	const course = {
+	  name: 'Half Stack application development',
+	  parts: [
+		{
+		  name: 'Fundamentals of React',
+		  exercises: 10,
+		  id: 1
+		},
+		{
+		  name: 'Using props to pass data',
+		  exercises: 7,
+		  id: 2
+		},
+		{
+		  name: 'State of a component',
+		  exercises: 14,
+		  id: 3
+		}
+	  ]
+	}
+
+	return (
+	  <div>
+		<Course course={course} />
+	  </div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
