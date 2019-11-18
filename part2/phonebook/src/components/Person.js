937274486784c3da1387aa49407e42bd8ef9d1bb
Kathy Daring <helgatheviking@gmail.com>
2019-11-18 13:22:53 -0700

    button doesn't need a value as the id is passed in the onClick function

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Person = ({person, removeHandler}) => {
 	return (
-	  <li>{person.name}: {person.number} <button value={person.id} onClick={() => removeHandler(person.id, person.name)}>Delete</button></li>
+	  <li>{person.name}: {person.number} <button onClick={() => removeHandler(person.id, person.name)}>Delete</button></li>
 	)
 }
 
